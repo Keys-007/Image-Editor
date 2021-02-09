3faf813d543c8f8e6122b8b99a924f1823ef4e5d
LEGENDARY KEYS <70282108+Keys-007@users.noreply.github.com>
2021-02-09 08:41:14 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -111,7 +111,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ],
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Image-Editor"
+                        "SOURCE CODE", url="https://github.com/TroHEX/Ime-Etor"
                     )
                 ],
             ]
@@ -131,7 +131,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ],
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Image-Editor"
+                        "SOURCE CODE", url="https://github.com/TroJaHEX/Iage-Etor"
                     )
                 ],
             ]
@@ -148,7 +148,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ],
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Image-Editor"
+                        "SOURCE CODE", url="https://github.com/TroJzHEX/Iage-Edtor"
                     )
                 ],
             ]
